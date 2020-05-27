6e19b4b276c2b4af6bf65d9e9623775d9e1f3e68
DSVtool <65762363+DSVtool@users.noreply.github.com>
2020-05-27 16:03:20 +0200

    Update ahb2apb_sequence.sv

--- ahb2apb_sequence.sv
+++ ahb2apb_sequence.sv
@@ -24,36 +24,17 @@ ahb_tr hseq;
 
 endclass
 
-class apb_seq extends base_sequence ;
-
-ahb_tr pseq;
-
-	`uvm_object_utils(apb_seq)
-
-	virtual task body();
-		`uvm_do_on(pseq, p_sequencer.apb_sqr);
-	endtask
-
-endclass
 
 class virtual_sequence extends base_sequence;	
 
 	`uvm_object_utils(virtual_sequence)
 
 	ahb_seq hseq;
-	apb_seq pseq;
 
 	virtual task body();
-
-		fork 
 			begin
 				`uvm_do(hseq);
 			end
-
-			begin
-				`uvm_do(pseq);
-			end
-		join	
 	endtask
 
 endclass
