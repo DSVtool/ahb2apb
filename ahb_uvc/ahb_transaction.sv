6ebb65edfa94d9ac0040746d9e65b2a20e7bbb63
DSVtool <65762363+DSVtool@users.noreply.github.com>
2020-05-29 09:58:03 +0200

    burst

--- ahb_uvc/ahb_transaction.sv
+++ ahb_uvc/ahb_transaction.sv
@@ -44,6 +44,7 @@ class ahb_tr #(parameter AHB_BUS_W = 32, AHB_ADDR_W = 32) extends uvm_sequence_i
     	hsize < $clog2(AHB_BUS_W)-2;
 		tr_delay inside {[0:60]};
 		soft hsel == 1;
+		soft hburst == 0;
 	}
 	
 	constraint undefburst_c {	
