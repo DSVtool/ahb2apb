1bc14073661ef002b5cfc5feea0636546b4bade0
DSVtool <65762363+DSVtool@users.noreply.github.com>
2020-05-29 12:28:41 +0200

    Update ahb_master_driver.sv

--- ahb_uvc/ahb_master_driver.sv
+++ ahb_uvc/ahb_master_driver.sv
@@ -90,9 +90,8 @@ task ahb_master_drv::drive();
 	int wrap_max, wrap_min, undefburst_lenght_local, haddr_temp;
 
 	`uvm_info("ahb driver", "hello-5", UVM_LOW)
-	repeat(req.tr_delay)
-		@(posedge vif.clk);  
-	`uvm_info("ahb driver", "hello-4", UVM_LOW)
+	repeat(req.tr_delay) #1;
+		`uvm_info("ahb driver", "hello-4", UVM_LOW)
 	if(req.hsel)
 		begin
 			`uvm_info("ahb driver", "hello-3", UVM_LOW)	
